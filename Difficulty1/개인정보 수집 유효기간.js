62d4a3440f725aac9a53520acad1a5cdef2eafd7
akthddus22 <48027227+akthddus22@users.noreply.github.com>
2023-03-30 22:30:02 +0900

    Create 개인정보 수집 유효기간.js

---
+++ Difficulty1/개인정보 수집 유효기간.js
@@ -0,0 +1,48 @@
+function parsing_days(date) {
+    var year = parseInt(date.substr(2, 2));
+    var month = parseInt(date.substr(5, 2));
+    var day = parseInt(date.substr(8, 2));
+    
+    return year*336 + (month-1)*28 + day;
+}
+
+function how_many_days(date, today) {
+    return parsing_days(today) - parsing_days(date);
+}
+
+function valid_day(terms, privacy) {
+    var specific_term = privacy.substr(11, 1);
+    for (var index in terms) {
+        var index_term = terms[index].substr(0, 1);
+        if (index_term == specific_term) {
+            var valid_month = parseInt(terms[index].substr(2));
+            return valid_month*28;
+        }
+    }
+    return 0;
+}
+
+function is_invalid(today, terms, privacy) {
+    
+    var date = privacy.substr(0,10);
+    
+    var limit = valid_day(terms, privacy);
+    var now = how_many_days(date, today);
+    console.log(limit, now);
+    
+    return now >= limit;
+}
+
+
+function solution(today, terms, privacies) {
+    var answer = [];
+    
+    for (var index in privacies) {
+        var privacy = privacies[index];
+        if (is_invalid(today, terms, privacy)) {
+            var invalid_privacy_index = parseInt(index)+1;
+            answer.push(invalid_privacy_index);
+        }
+    }
+    return answer;
+}
